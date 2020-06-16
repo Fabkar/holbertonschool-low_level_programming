0f351fee22928bd2354b3672d2c813bf77beceea
Fabian Carmona <2003@holbertonschool.com>
2020-06-16 16:41:58 +0000

    Library Holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,6 @@
-/*Protipo _putchar*/
+/*Prototype _putchar*/
 int _putchar (char c);
-/*Prototipo to print alphabet*/
+/*Prototype to print alphabet*/
 void print_alphabet(void);
+/*Prototype to print 10 times the alphabet*/
+void print_alphabet_x10(void);
