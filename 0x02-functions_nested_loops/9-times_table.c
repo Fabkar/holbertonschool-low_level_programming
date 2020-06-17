e3277ae1eba149c69de8058113a9d17763d98f37
Fabian Carmona <2003@holbertonschool.com>
2020-06-17 01:05:09 +0000

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ *times_table - Print table
+ *
+ *Return: Always 0.
+ **/
+void times_table(void)
+{
+	int h, v;
+	int n = 0;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (v = 0; v <= 9; v++)
+		{
+			n = h * v;
+			if (n > 9)
+			{
+				_putchar ((n / 10) + '0');
+				_putchar ((n % 10) + '0');
+				if (v <= 8)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+			}
+			if (n <= 9)
+			{
+				_putchar (n + '0');
+				if (v <= 8)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
+			}
+		}
+		_putchar ('\n');
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -16,3 +16,4 @@ int _abs(int);
 int print_last_digit(int);
 /*Jack_bauer*/
 void jack_bauer(void);
+void times_table(void);
