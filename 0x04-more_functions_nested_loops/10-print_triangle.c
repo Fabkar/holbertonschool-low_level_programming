5ceea4fea31e1ebd01fda6d93e2f73c085f0f489
Fabian Carmona <2003@holbertonschool.com>
2020-06-19 21:41:37 +0000

    Print a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *print_triangle - Print a Triangle.
+ *@size: integer.
+ *Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int f, c;
+
+	for (f = 0; f < size; f++)
+	{
+		for (c = (size - 1); c > f; c--)
+		{
+			_putchar(' ');
+		}
+		for (c = 0; c <= f; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
