120a42e3aa61e5dc01050a16b427f660b6ae430d
Fabian Carmona <2003@holbertonschool.com>
2020-06-23 16:50:51 +0000

    Swap between two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *swap_int - Swaps the values of tow integers.
+ *@a:Pointer to b.
+ *@b:Pointer to a.
+ *Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
