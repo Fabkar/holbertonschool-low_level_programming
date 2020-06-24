d63df8ad3f9125a34679b39211c64b3c5183822d
Fabian Carmona <2003@holbertonschool.com>
2020-06-24 20:41:45 +0000

    Header prototype

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define HOLBERTON_H
+void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void puts_half(char *str);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+#endif
