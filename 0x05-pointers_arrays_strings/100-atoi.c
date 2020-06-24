33e21ee12b30df2f061b1cdd73bdd45e7a8fd6c6
Fabian Carmona <2003@holbertonschool.com>
2020-06-24 23:20:55 +0000

    Function that convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _atoi - Convert a string to an integer
+ *@s: pointer.
+ *Return: integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+		       sign = sign * -1;
+		}
+		if ((*s >= '0') && (*s <= '9'))
+		{
+			num = ((num * 10) + (*s - '0'));
+		}
+		if (*s == ';')
+		{
+			break;
+		}
+		s++;
+	}
+	return(num * sign);
+}
