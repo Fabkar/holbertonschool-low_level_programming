eaa45e6a9346ba2299952082929e87069a270076
Fabian Carmona <2003@holbertonschool.com>
2020-06-25 01:08:44 +0000

    Print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - Function thet reverses a string.
+ * @s: Pointer.
+ *Return: nothing.
+ */
+void print_rev(char *s)
+{
+	int leng = 0;
+	while (*(s + leng) != '\0')
+	{
+		leng++;
+	}
+	leng = leng -1;
+	while (leng >= 0)
+	{
+		_putchar(*(s + leng));
+		leng--;
+	}
+	_putchar('\n');
+}
