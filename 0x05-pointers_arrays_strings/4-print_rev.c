57ae84f6f895c326ed9c1e1726d55c854a7e6b98
Fabian Carmona <2003@holbertonschool.com>
2020-06-25 01:12:43 +0000

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,12 @@
 void print_rev(char *s)
 {
 	int leng = 0;
+
 	while (*(s + leng) != '\0')
 	{
 		leng++;
 	}
-	leng = leng -1;
+	leng = leng - 1;
 	while (leng >= 0)
 	{
 		_putchar(*(s + leng));
