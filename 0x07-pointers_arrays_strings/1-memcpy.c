cdec4996ad16dc1adc7fff606dd0e60fc572b8a6
Fabian Carmona <2003@holbertonschool.com>
2020-06-29 00:37:43 +0000

    copy set memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int c = 0;
+
+	while (c < n)
+	{
+		dest[c] = src[c];
+		c++;
+	}
+	return (dest);
+}
