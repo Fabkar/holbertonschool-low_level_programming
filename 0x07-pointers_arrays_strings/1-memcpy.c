742e139a5cbc4b7779033c62e895715b61a56f61
Fabian Carmona <2003@holbertonschool.com>
2020-06-29 00:43:02 +0000

    Function documentation

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _memcpy - Copy n characters from memory area src to memory area dest.
+ * @dest: Pointer to the destination array where the content will be copied
+ * @src: Pointer to the source of data to be copied.
+ * @n: Number of bytes to be copied.
+ * Return: Pointer dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
