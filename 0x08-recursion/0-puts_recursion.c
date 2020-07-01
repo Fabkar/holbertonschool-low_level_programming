8ca299ef7e497d8f772656742c7653b38b645aa3
Fabian Carmona <2003@holbertonschool.com>
2020-07-01 20:12:39 +0000

    First Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - Print a string.
+ * @s: string.
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar(10);
+}
