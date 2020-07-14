830a1b81899da23f8df50f87958154f2f337d18c
Fabian Carmona <2003@holbertonschool.com>
2020-07-14 03:07:28 +0000

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef STRUCT_MY_DOG
-#define TRUCT_MY_DOG
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - struct to define my dog.
  * @name:  dog name.
@@ -11,8 +11,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#endif/*dog*/
+#endif
