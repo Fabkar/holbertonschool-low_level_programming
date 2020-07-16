fa93af63236932951f8594cc81d71316d9e6195e
Fabkar <2003@holbertonschool.com>
2020-07-16 18:07:30 -0500

    operator of function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+* op_add - sum of two integer
+* @a: integer 1.
+* @b: integer 2.
+* Return: int.
+*/
+int op_add(int a, int b) {return (a + b); }
+/**
+* op_sub - subtract between two integers.
+* @a: integer 1.
+* @b: integer 2.
+* Return: int.
+*/
+int op_sub(int a, int b) {return (a - b); }
+/**
+* op_mul - multiply two integers.
+* @a: integer 1.
+* @b: integer 2.
+* Return: int.
+*/
+int op_mul(int a, int b) {return (a * b); }
+/**
+* op_div - division two integers.
+* @a: integer 1.
+* @b: integer 2.
+* Return: int.
+*/
+int op_div(int a, int b) {return (a / b); }
+/**
+* op_mod - module of two integers.
+* @a: integer 1.
+* @b: integer 2.
+* Return: int.
+*/
+int op_mod(int a, int b) {return (a % b); }
