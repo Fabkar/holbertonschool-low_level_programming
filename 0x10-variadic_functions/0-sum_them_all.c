c876eb1a95fecfb58cc029e0a82953099cfc51a4
Fabkar <2003@holbertonschool.com>
2020-07-17 19:51:52 -0500

    fix intendention

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,19 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list op_sum;
-    int sum = 0;
-    unsigned int i;
-    va_start(op_sum, n);
-    
-    if (n == 0)
-        return (0);
-    
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(op_sum, int);
-    }
-    va_end(op_sum);
-    return (sum);
+	va_list op_sum;
+	int sum = 0;
+	unsigned int i;
+
+	va_start(op_sum, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(op_sum, int);
+	}
+	va_end(op_sum);
+	return (sum);
 }
