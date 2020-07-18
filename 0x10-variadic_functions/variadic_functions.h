a6cd3e93ff1ffdda50782b99fde667d82220c37c
Fabkar <2003@holbertonschool.com>
2020-07-18 12:05:36 -0500

    Header Prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,24 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* struct formats - struct formats
+* @ch: pointer to firts element.
+* @func: The function associated.
+*/
+typedef struct formats
+	{
+		char *ch;
+		void (*func)(va_list);
+	} formats;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void character(va_list list);
+void integer(va_list list);
+void floating(va_list list);
+void string(va_list list);
 #endif/*_VARIADIC_FUNCTIONS_H*/
