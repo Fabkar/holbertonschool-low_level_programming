b7bba366ad1f56d844b742927fd985fc8515b852
Fabkar <2003@holbertonschool.com>
2020-07-27 15:19:25 -0500

    Add Header Prototypes and function to prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* print_listint - prints all the elements of a listint_t list.
+* @h: head pointer.
+* Return: Number of nodes.
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u]\n", (*h).n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
