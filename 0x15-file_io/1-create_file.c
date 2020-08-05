9aef8bf82fa528f115a7817b9610d33e4c56004d
Fabkar <2003@holbertonschool.com>
2020-08-04 22:42:56 -0500

    update function 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,14 +12,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		text_content "";
+		text_content = "";
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 	{
 		close(fd);
 		return (0);
 	}
-	for (i = 0; text_content[i], i++)
+	for (i = 0; text_content[i]; i++)
 	;
 	c = write(fd, text_content, i);
 	if (c == -1)
