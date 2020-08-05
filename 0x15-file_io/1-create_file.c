f5f2fcd8eca151f929c6d8f0082e7e770b3aa4bd
Fabkar <2003@holbertonschool.com>
2020-08-04 22:39:31 -0500

    update header and add function to create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+* create_file - fucntion to creat a text file.
+* @filename: poiter to filename.
+* @text_content: tex_content.
+* Return: integer.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, c, i;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content "";
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	for (i = 0; text_content[i], i++)
+	;
+	c = write(fd, text_content, i);
+	if (c == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
